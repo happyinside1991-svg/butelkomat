9fd0118bd4d1448afc1215cf0ab9310bdfa1dab6
happyinside1991-svg <happyinside1991@gmail.com>
2026-08-10 15:08:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,22 +16,20 @@ function scan() {
     count++;
     money += 0.50;
 
+    const moneyText = money.toFixed(2).replace(".", ",") + " zł";
+
     document.getElementById("status").innerHTML =
       "🟢<br>ZWROT PRZYJĘTY!";
 
     document.getElementById("count").textContent = count;
-
-    const moneyText =
-      money.toFixed(2).replace(".", ",") + " zł";
-
     document.getElementById("money").textContent = moneyText;
 
     document.getElementById("receiptCount").textContent = count;
     document.getElementById("receiptTotal").textContent = moneyText;
     document.getElementById("receiptTotalBig").textContent = moneyText;
-
     document.getElementById("receiptDate").textContent =
       new Date().toLocaleString("pl-PL");
+
   } else {
     document.getElementById("status").innerHTML =
       "🔴<br>NIEPRAWIDŁOWE OPAKOWANIE";
@@ -39,5 +37,13 @@ function scan() {
 }
 
 function drukujBon() {
+  const moneyText = money.toFixed(2).replace(".", ",") + " zł";
+
+  document.getElementById("receiptCount").textContent = count;
+  document.getElementById("receiptTotal").textContent = moneyText;
+  document.getElementById("receiptTotalBig").textContent = moneyText;
+  document.getElementById("receiptDate").textContent =
+    new Date().toLocaleString("pl-PL");
+
   window.print();
 }
